54539964dc299b05893aff9ee185aa23491355a2
Basil Arnander <nk21253@bristol.ac.uk>
2023-02-14 10:23:04 +0000

    epsilon decreasing class, new main class to run different strategies and produce plots, file exchange code for shaded error plots from Víctor Martínez-Cagigal.

---
+++ eGreedy_run.m
@@ -0,0 +1,46 @@
+classdef eGreedy_run <handle
+    %main class is used to run the loop over n trials
+    %will use eGreedy and also ad_Bandit classes
+
+    %the easiest way to run this code is to create an instance of the main
+    %class (i.e. x = main). Then x.run in the command window will produce
+    %a table of the scores and a plot to visualise what is happening
+    
+    properties 
+        ad_Campaigns
+        myGreedy
+        n_trials
+        regret
+    end
+
+
+    methods
+        function obj = eGreedy_run() %initialise object
+            obj.ad_Campaigns = ad_Bandit([0.28,0.35,0.4]); %bandit should have probabilities passed into it - if not will still run (see inside this class)
+            obj.myGreedy = eGreedy(obj.ad_Campaigns.N, 0.1); %inputs to eGreedy are number of ad campaigns and value of epsilon 
+            obj.n_trials = 1000;
+            obj.regret = [];
+        end
+    
+
+        function run(obj)
+            ad_names = string( (1:obj.ad_Campaigns.N) );
+            success = 0;
+            
+            for i = 1: obj.n_trials-1
+                ad_choice = obj.myGreedy.choose();
+                click = obj.ad_Campaigns.test(ad_choice);
+                success = success + click;
+
+                obj.myGreedy = obj.myGreedy.update(ad_choice, click);
+                obj.regret(end+1) = success - i*max(obj.ad_Campaigns.probs);
+                
+                scores = obj.myGreedy.scores;
+                scores_table = array2table(scores);
+                scores_table.Properties.VariableNames = ad_names;
+               
+            end
+        end
+    end
+
+end
--- main.m
+++ main.m
@@ -1,55 +1,63 @@
-classdef main <handle
-    %main class is used to run the loop over n trials
-    %will use eGreedy and also ad_Bandit classes
-
-    %the easiest way to run this code is to create an instance of the main
-    %class (i.e. x = main). Then x.run in the command window will produce
-    %a table of the scores and a plot to visualise what is happening
-    
-    properties 
-        ad_Campaigns
-        myGreedy
-        n_trials   
-    end
+% run eGreedy model + plots
+e_Greedy = eGreedy_run;
+e_Greedy.run
 
 
-    methods
-        function obj = main() %initialise object
-            obj.ad_Campaigns = ad_Bandit([0.28,0.35,0.4]); %bandit should have probabilities passed into it - if not will still run (see inside this class)
-            obj.myGreedy = eGreedy(obj.ad_Campaigns.N, 0.2); %inputs to eGreedy are number of ad campaigns and value of epsilon 
-            obj.n_trials = 1000;  
-        end
-    
-
-        function run(obj)
-            ad_names = string( (1:obj.ad_Campaigns.N) );
+figure
+hold on
+x = 1:e_Greedy.n_trials;
             
-            for i = 1: obj.n_trials-1
-                ad_choice = obj.myGreedy.choose();
-                click = obj.ad_Campaigns.test(ad_choice);
-
-                obj.myGreedy = obj.myGreedy.update(ad_choice, click);
-                
-                scores = obj.myGreedy.scores;
-                scores_table = array2table(scores);
-                scores_table.Properties.VariableNames = ad_names;
-               
+for i = 1:e_Greedy.ad_Campaigns.N
+    plot(x, [e_Greedy.myGreedy.scores(:,i)]', 'LineWidth',2);
+    xlabel('number of trials'); ylabel('expected reward'); 
+    title('Epsilon-greedy strategy for Ad Campaigns');
             end
-            disp(scores_table); %outputs table of scores where each row is a consecutive trial
-            
-            %following part simply plots a graph of the scores we have
-            %obtained above against the number of trials. 
-            hold on
-            x = 1:obj.n_trials;
-            
-            for i = 1:obj.ad_Campaigns.N
-                plot(x, [scores(:,i)]', 'LineWidth',2);
-                xlabel('number of trials'); ylabel('expected reward'); 
-                title('Eepsilon-greedy strategy for Ad Campaigns');
-            end
-            hold off 
-            
-        end
-    end
+legend(["Ad1", "Ad2", "Ad3"])
+hold off
 
+figure 
+plot(1:e_Greedy.n_trials - 1, e_Greedy.regret)
+xlabel("Trial")
+ylabel("Regret")
+title("Regret over 1000 trials")
+
+%% plots regret for different epsilon values for eGreedy
+epsilons = 0:0.05: 0.25;
+x = 1:10;
+
+figure
+hold on
+for epsilon = epsilons
+    for i = x
+        e_Greedy = eGreedy_run;
+        e_Greedy.myGreedy.epsilon = epsilon;
+        e_Greedy.run
+        regrets(i,:) = e_Greedy.regret;
+    end
+    
+    plot(mean(regrets))
+    epsilon
 end
+xlabel("#Ads Shown")
+ylabel("Regret")
+legend(string(epsilons))
+hold off
+%% average regret over 10 experiments of 1000 trials and plot egreedy
+regrets_greedy = [];
+regrets_decreasing = [];
+x = 1:100;
+for i = x
+    e_Greedy = eGreedy_run;
+    e_Greedy.run
+    regrets_greedy(i,:) = e_Greedy.regret;
+
+    e_Decrease = eDecrease_run;
+    e_Decrease.run
+    regrets_decreasing(i,:) = e_Decrease.regret;
+end
+
+plot_areaerrorbar(regrets_greedy)
+
+%% plot edecreasing
+
+plot_areaerrorbar(regrets_decreasing)
